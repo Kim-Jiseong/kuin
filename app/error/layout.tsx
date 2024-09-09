efb93b98a3b4deaffae3ca439cd6f95a2065d26d
Kim Jiseong <35622664+Kim-Jiseong@users.noreply.github.com>
2024-09-09 23:26:08 +0900

    migrate to supabase auth, change EVERYTHING in server component

---
+++ app/error/layout.tsx
@@ -0,0 +1,38 @@
+"use client";
+import "@/styles/globals.css";
+import { Metadata, Viewport } from "next";
+import { Link } from "@nextui-org/link";
+import clsx from "clsx";
+
+import { Providers } from "../providers";
+
+import { siteConfig } from "../../config/site";
+import { fontSans } from "../../config/fonts";
+import { Navbar } from "../../components/navbar";
+import { Suspense } from "react";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html suppressHydrationWarning lang="en">
+      <head />
+      <body
+        className={clsx(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+          <div className="relative flex flex-col h-screen">
+            <Suspense>
+              <main className="container mx-auto flex-grow">{children}</main>
+            </Suspense>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  );
+}
