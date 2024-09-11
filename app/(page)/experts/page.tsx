074d2c5f0458f6beab15b31d8025308dc744d479
Kim Jiseong <35622664+Kim-Jiseong@users.noreply.github.com>
2024-09-11 19:11:14 +0900

    expert page finished

--- app/(page)/experts/page.tsx
+++ app/(page)/experts/page.tsx
@@ -2,7 +2,7 @@
 
 import { major as majorList } from "@/constant/major";
 import { returnMajorColor } from "@/utils/returnMajorColor";
-import { Button, Input, Link, Tab, Tabs } from "@nextui-org/react";
+import { Button, Input, Link, Spinner, Tab, Tabs } from "@nextui-org/react";
 import { useEffect, useState } from "react";
 import { Key } from "@react-types/shared";
 import { Tables } from "@/types/database.types";
@@ -18,6 +18,7 @@ export default function ExpertsPage() {
   const [searchQuery, setSearchQuery] = useState<string>("");
   const [myProfile, setMyProfile] = useState<Tables<"profile">>();
   const [profileList, setProfileList] = useState<Tables<"profile">[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   const getMy = async () => {
     const myProfile = await getMyProfile();
@@ -26,6 +27,7 @@ export default function ExpertsPage() {
     }
   };
   const getProfileList = async (query: string) => {
+    setIsLoading(true);
     const { data, error } = await supabase.rpc("search_expert_profiles", {
       major_filter: major,
       search_text: query,
@@ -33,6 +35,7 @@ export default function ExpertsPage() {
     if (data) {
       setProfileList(data);
     }
+    setIsLoading(false);
   };
 
   const handleClickClear = () => {
@@ -63,13 +66,17 @@ export default function ExpertsPage() {
       {myProfile?.expert_profile === null && (
         <div
           className={
-            " p-2 bg-primary-100 border-1 border-primary-500 text-center break-keep text-sm flex items-center justify-center gap-4 rounded-lg "
+            "w-full max-w-xl mx-auto py-2 px-4 bg-primary-100 border-1 border-primary-500 text-center break-keep text-sm flex items-center justify-center gap-4 rounded-lg "
           }
         >
           <p>
             아직 전문가 프로필이 없으시네요! 전문가 프로필을 등록하시겠어요?
           </p>
-          <Link size="sm" href={"/experts/edit/" + myProfile?.id}>
+          <Link
+            size="sm"
+            underline="always"
+            href={"/experts/edit/" + myProfile?.id}
+          >
             등록하기
           </Link>
         </div>
@@ -106,18 +113,29 @@ export default function ExpertsPage() {
         </Tabs>
       </div>
       <div className={"w-full flex flex-wrap gap-4 mt-4 pb-4"}>
-        {profileList.length > 0 ? (
-          profileList.map((profile) => (
-            <ExpertProfileDisplayCard key={profile.id} profile={profile} />
-          ))
+        {!isLoading ? (
+          profileList.length > 0 ? (
+            profileList.map((profile) => (
+              <ExpertProfileDisplayCard key={profile.id} profile={profile} />
+            ))
+          ) : (
+            <div
+              className={
+                "mx-auto flex flex-col items-center justify-center gap-2 my-5"
+              }
+            >
+              <Frown size={96} />
+              <Typography variant={"text"}>검색결과가 없습니다</Typography>
+            </div>
+          )
         ) : (
           <div
             className={
-              "mx-auto flex flex-col items-center justify-center gap-2 my-5"
+              "mx-auto flex flex-col items-center justify-center gap-2 my-10"
             }
           >
-            <Frown size={96} />
-            <Typography variant={"text"}>검색결과가 없습니다</Typography>
+            {/* <Typography variant={"text"}>로딩중입니다</Typography> */}
+            <Spinner />
           </div>
         )}
       </div>
